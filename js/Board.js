db57e1772f6ee1b257a8811c85800a688fda0a40
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-01-06 22:24:13 +0100

    Added move card functionality and changed style

--- js/Board.js
+++ js/Board.js
@@ -10,21 +10,46 @@ var board = {
 $('.create-column')
     .click(function() {
         var columnName = prompt('Enter a column name');
-        $.ajax({
-    		url: baseUrl + '/column',
-    		method: 'POST',
-    		data: {
-            	name: columnName
-    		},
-    		success: function(response){
-    			var column = new Column(response.id, columnName);
-    			board.createColumn(column);
-          	}
-        });
+        if (columnName !== null) {
+        	$.ajax({
+	    		url: baseUrl + '/column',
+	    		method: 'POST',
+	    		data: {
+	            	name: columnName
+	    		},
+	    		success: function(response){
+	    			var column = new Column(response.id, columnName);
+	    			board.createColumn(column);
+	          	}
+        	});	
+        }
 });
 function initSortable() {
     $('.card-list').sortable({
       connectWith: '.card-list',
-      placeholder: 'card-placeholder'
+      placeholder: 'card-placeholder',
+      update: function(event, ui) {
+      	if (ui.sender) {
+      		var card = ui.item;
+      		var cardId = card.attr('data-card-id');
+      		var cardName = card.find('.card-description').text();
+
+      		var columnId = card.parent()
+      		.parent()
+      		.attr('data-column-id');
+      	
+	      	$.ajax({
+	      		url: baseUrl + '/card/' + cardId,
+	      		method: 'PUT',
+	      		data: {
+	      			name: cardName,
+	      			bootcamp_kanban_column_id: columnId
+	      		},
+	      		success: function(response) {
+	      			// console.log('hurra! ' + response.id);
+	      		}
+	      	});
+      	}
+      }
     }).disableSelection();
   }
--- js/Column.js
+++ js/Column.js
@@ -7,8 +7,8 @@ function Column(id, name) {
 
 	function createColumn() {
 		// TWORZENIE NOWYCH WĘZŁÓW
-		var column = $('<div class="column"></div>');
-		var columnTitle = $('<h2 class="column-title">' + self.name + '</h2>');
+		var column = $('<div class="column" data-column-id="' + self.id + '"></div>');
+		var columnTitle = $('<h2 class="column-title">' + self.name +'</h2>');
 		var columnCardList = $('<ul class="card-list"></ul>');
 		var columnDelete = $('<button class="btn-delete">x</button>');
 		var columnAddCard = $('<button class="column-add-card">Dodaj kartę</button>');
@@ -20,19 +20,21 @@ function Column(id, name) {
 		
 		columnAddCard.click(function(event) {
 			var cardName = prompt("Enter the name of the card");
-			event.preventDefault();
-			$.ajax({
-			    url: baseUrl + '/card',
-			    method: 'POST',
-			    data: {
-			    name: cardName,
-			    bootcamp_kanban_column_id: self.id
-			    },
-			    success: function(response) {
-			        var card = new Card(response.id, cardName);
-			        self.createCard(card);
-			    }
-			});
+		    if (cardName !== null) {
+			    event.preventDefault();
+				$.ajax({
+				    url: baseUrl + '/card',
+				    method: 'POST',
+				    data: {
+				    name: cardName,
+				    bootcamp_kanban_column_id: self.id
+				    },
+				    success: function(response) {
+				        var card = new Card(response.id, cardName);
+				        self.createCard(card);
+				    }
+				});
+			}
 		});
 			
 			// KONSTRUOWANIE ELEMENTU KOLUMNY
