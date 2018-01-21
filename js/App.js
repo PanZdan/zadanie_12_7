73f5c566fe41f3c40ba2c97a8d476f3a97567653
Konrad Zdanowski <konrad-zdanowski@wp.pl>
2018-01-21 14:53:30 +0100

    Changed style and indentation on all files

--- js/App.js
+++ js/App.js
@@ -7,28 +7,27 @@ var myHeaders = {
 };
 
 $.ajaxSetup({
-	headers: myHeaders
+  headers: myHeaders
 });
 
 $.ajax({
-    url: baseUrl + '/board',
-    method: 'GET',
-    success: function(response) {
-      setupColumns(response.columns);
-    }
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function(response) {
+    setupColumns(response.columns);
+  }
 });
 
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-        board.createColumn(col);
-        setupCards(col, column.cards);
-    });
+  columns.forEach(function (column) {
+    var col = new Column(column.id, column.name);
+      board.createColumn(col);
+      setupCards(col, column.cards);
+  });
 }
-
 function setupCards(col, cards) {
-	cards.forEach(function (card) {
-        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.createCard(card);
-  	})
+  cards.forEach(function (card) {
+    var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    col.createCard(card);
+  })
 }
--- js/Board.js
+++ js/Board.js
@@ -1,55 +1,55 @@
 var board = {
-	name: 'Tablica Kanban',
-	createColumn: function(column) {
-	  this.element.append(column.element);
-	  initSortable();
-	},
-	element: $('#board .column-container')
+  name: 'Tablica Kanban',
+  createColumn: function(column) {
+    this.element.append(column.element);
+    initSortable();
+  },
+  element: $('#board .column-container')
 };
 
 $('.create-column')
-    .click(function() {
-        var columnName = prompt('Enter a column name');
-        if (columnName !== null) {
-        	$.ajax({
-	    		url: baseUrl + '/column',
-	    		method: 'POST',
-	    		data: {
-	            	name: columnName
-	    		},
-	    		success: function(response){
-	    			var column = new Column(response.id, columnName);
-	    			board.createColumn(column);
-	          	}
-        	});	
+  .click(function() {
+    var columnName = prompt('Enter a column name');
+    if (columnName !== null) {
+      $.ajax({
+        url: baseUrl + '/column',
+        method: 'POST',
+        data: {
+         name: columnName
+        },
+        success: function(response){
+          var column = new Column(response.id, columnName);
+          board.createColumn(column);
         }
-});
-function initSortable() {
+      }); 
+    }
+  });
+  function initSortable() {
     $('.card-list').sortable({
       connectWith: '.card-list',
       placeholder: 'card-placeholder',
       update: function(event, ui) {
-      	if (ui.sender) {
-      		var card = ui.item;
-      		var cardId = card.attr('data-card-id');
-      		var cardName = card.find('.card-description').text();
+        if (ui.sender) {
+          var card = ui.item;
+          var cardId = card.attr('data-card-id');
+          var cardName = card.find('.card-description').text();
 
-      		var columnId = card.parent()
-      		.parent()
-      		.attr('data-column-id');
-      	
-	      	$.ajax({
-	      		url: baseUrl + '/card/' + cardId,
-	      		method: 'PUT',
-	      		data: {
-	      			name: cardName,
-	      			bootcamp_kanban_column_id: columnId
-	      		},
-	      		success: function(response) {
-	      			// console.log('hurra! ' + response.id);
-	      		}
-	      	});
-      	}
+          var columnId = card.parent()
+          .parent()
+          .attr('data-column-id');
+        
+          $.ajax({
+            url: baseUrl + '/card/' + cardId,
+            method: 'PUT',
+            data: {
+              name: cardName,
+              bootcamp_kanban_column_id: columnId
+            },
+            success: function(response) {
+              // console.log('hurra! ' + response.id);
+            }
+          });
+        }
       }
     }).disableSelection();
   }
